0a6b3696bd387b1d2d9b7bb9f4827667b47798a8
Pedro-jferreira <pedrohenriquedejesusferreira13@gmail.com>
2025-05-13 08:30:55 -0300

    initial commit

---
+++ src/main/java/com/example/pub_sub_queue/PubSubQueueApplication.java
@@ -0,0 +1,13 @@
+package com.example.pub_sub_queue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PubSubQueueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PubSubQueueApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/pub_sub_queue/PubSubQueueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pub_sub_queue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PubSubQueueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
