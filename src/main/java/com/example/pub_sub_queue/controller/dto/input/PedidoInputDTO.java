837decd0074fc9bbab36141264325c1b7172bf5c
arianamesquita <arianamesquita85@gmail.com>
2025-05-30 09:03:42 -0300

    develop: ajustes de getter e setter no pedidoinputdto

--- src/main/java/com/example/pub_sub_queue/controller/dto/input/PedidoInputDTO.java
+++ src/main/java/com/example/pub_sub_queue/controller/dto/input/PedidoInputDTO.java
@@ -13,4 +13,46 @@ public class PedidoInputDTO extends SimplePedidoDTO {
     private LocalDateTime dataEntrega;
     private String descricao;
     private String codigo;
+
+    public LocalDateTime getDataPedido() {
+        return dataPedido;
+    }
+
+    public void setDataPedido(LocalDateTime dataPedido) {
+        this.dataPedido = dataPedido;
+    }
+
+    public LocalDateTime getDataEntrega() {
+        return dataEntrega;
+    }
+
+    public void setDataEntrega(LocalDateTime dataEntrega) {
+        this.dataEntrega = dataEntrega;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    @Override
+    public String toString() {
+        return "PedidoInputDTO{" +
+                "dataPedido=" + dataPedido +
+                ", dataEntrega=" + dataEntrega +
+                ", descricao='" + descricao + '\'' +
+                ", codigo='" + codigo + '\'' +
+                '}';
+    }
 }
