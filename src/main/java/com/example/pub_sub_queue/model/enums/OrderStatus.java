ebef26c8b2d1b0578885306c2362da707c479590
Pedro-jferreira <pedrohenriquedejesusferreira13@gmail.com>
2025-05-30 08:56:48 -0300

    :recycle: refactor() OrderStatus  updated

--- src/main/java/com/example/pub_sub_queue/model/enums/OrderStatus.java
+++ src/main/java/com/example/pub_sub_queue/model/enums/OrderStatus.java
@@ -1,12 +1,14 @@
 package com.example.pub_sub_queue.model.enums;
 
-public enum PedidoStatus {
-    PEDIDO_PROCESSADO(1),
-    PEDIDO_ENTREGUE(2);
+public enum OrderStatus {
+    PEDIDO_CRIADO(1),
+    PEDIDO_PROCESSADO(2),
+    PEDIDO_TRANSPORTE(3),
+    PEDIDO_ENTREGUE(4);
 
     private final int status;
 
-    PedidoStatus(int status) {
+    OrderStatus(int status) {
         this.status = status;
     }
 
