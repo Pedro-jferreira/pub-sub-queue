23cdceb0e061d92dad5d94ca23cb5943ed5449f8
Pedro-jferreira <pedrohenriquedejesusferreira13@gmail.com>
2025-05-30 09:00:46 -0300

    :sparkles: feat(AuditConsumer) Consumer Audit created

---
+++ src/main/java/com/example/pub_sub_queue/consumer/AuditConsumer.java
@@ -0,0 +1,21 @@
+package com.example.pub_sub_queue.consumer;
+
+import com.example.pub_sub_queue.model.InternalAudit;
+import com.example.pub_sub_queue.model.Order;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.function.Consumer;
+
+@Configuration
+public class AuditConsumer {
+    private InternalAudit internalAudit =  InternalAudit.builder().build();
+
+    @Bean
+    public Consumer<Order> auditoriaPedidoProcessado() {
+        return pedido -> {
+            internalAudit.addOrder(pedido);
+            System.out.println("📝 Auditando pedido: " + pedido.getId());
+        };
+    }
+}
